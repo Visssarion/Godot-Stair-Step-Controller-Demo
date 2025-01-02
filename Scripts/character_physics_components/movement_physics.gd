2b64f3ae0b98265a26dcde745d3c22b2f5bc1d76
vissa <vlad5limar@gmail.com>
2025-01-02 21:51:34 +0300

    Add camera component

--- Scripts/character_physics_components/movement_physics.gd
+++ Scripts/character_physics_components/movement_physics.gd
@@ -5,6 +5,7 @@ extends Node
 @export var speed: float = 10
 
 func _physics_process(delta: float) -> void:
-	var wish_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var wish_dir := (character.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	character.velocity.x = wish_dir.x * speed
-	character.velocity.z = wish_dir.y * speed
+	character.velocity.z = wish_dir.z * speed
